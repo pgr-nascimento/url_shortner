432ce116912e1f6f1d6fe8ae5eb4f3f96c96b5ab
Alan Nascimento <alanoliveiran45@gmail.com>
2021-11-15 21:53:45 -0300

    chore: Add way to have a conn with an initialized session
    I was having problem with how to put the ids on the session and I found a gist telling
    how to solve it: https://gist.github.com/tsubery/2e7d118a51702c1e24741b9e2d9dfdbf

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -28,6 +28,10 @@ defmodule UrlShortnerWeb.ConnCase do
 
       # The default endpoint for testing
       @endpoint UrlShortnerWeb.Endpoint
+
+      def session_conn() do
+        build_conn() |> Plug.Test.init_test_session(%{})
+      end
     end
   end
 
