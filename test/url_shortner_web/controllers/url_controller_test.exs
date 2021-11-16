c8ed0da361ca957cff10a9cd2d05f08401ce4c16
Alan Nascimento <alanoliveiran45@gmail.com>
2021-11-15 23:18:51 -0300

    chore: Add a scenario test to list the URLs

--- test/url_shortner_web/controllers/url_controller_test.exs
+++ test/url_shortner_web/controllers/url_controller_test.exs
@@ -7,6 +7,21 @@ defmodule UrlShortnerWeb.UrlControllerTest do
 
   import Ecto.Query
 
+  test "without URLs created by the user, do not see others URLs" do
+    {:ok, %{id: first_id}} = Repo.insert(%Url{original_url: "https://xkcd.com", slug: "xkcd327"})
+
+    response =
+      session_conn()
+      |> put_session(:urls, [first_id])
+      |> get("/")
+      |> html_response(:ok)
+
+    assert response =~ "https://xkcd.com"
+    assert response =~ "xkcd327"
+    assert response =~ "https://xkcd.com"
+    assert response =~ "ggle308"
+  end
+
   test "See a list with all urls created by the user" do
     {:ok, %{id: first_id}} = Repo.insert(%Url{original_url: "https://xkcd.com", slug: "xkcd327"})
 
