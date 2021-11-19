5c5b2190e05c5aa9204017d9ec7defa909d86aad
Alan Nascimento <alanoliveiran45@gmail.com>
2021-11-19 00:41:20 -0300

    feat: Update the UrlView
    - Add tests
    - Add documentation
    - Remove unnecessary fn

--- lib/url_shortner_web/views/url_view.ex
+++ lib/url_shortner_web/views/url_view.ex
@@ -1,10 +1,9 @@
 defmodule UrlShortnerWeb.UrlView do
   use UrlShortnerWeb, :view
 
-  def build_link(conn, slug) do
-    Routes.url_path(conn, :show, slug)
-  end
-
+  @doc """
+  This fn returns the host + the slug of the URL to show to the user the URL shortned
+  """
   def build_link_text(slug) do
     host = Application.get_env(:url_shortner, UrlShortnerWeb.Endpoint)[:url][:host]
     "#{host}/#{slug}"
