a3dd5509cba7b06bfbf17491ed8ff639189fa805
Alan Nascimento <alanoliveiran45@gmail.com>
2021-11-19 00:25:57 -0300

    feat: Use the config value to build the URL links

--- lib/url_shortner_web/views/url_view.ex
+++ lib/url_shortner_web/views/url_view.ex
@@ -1,3 +1,12 @@
 defmodule UrlShortnerWeb.UrlView do
   use UrlShortnerWeb, :view
+
+  def build_link(conn, slug) do
+    Routes.url_path(conn, :show, slug)
+  end
+
+  def build_link_text(slug) do
+    host = Application.get_env(:url_shortner, UrlShortnerWeb.Endpoint)[:url][:host]
+    "#{host}/#{slug}"
+  end
 end
