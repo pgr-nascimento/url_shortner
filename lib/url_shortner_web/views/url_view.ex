e5ce284ee6f01004b0a5fad2d86fc778b347e07b
Alan Nascimento <alanoliveiran45@gmail.com>
2021-11-25 23:47:39 -0300

    feat: add format_original_url/1 fn
    This fn is a helper to show the original url in a better way for the users.

--- lib/url_shortner_web/views/url_view.ex
+++ lib/url_shortner_web/views/url_view.ex
@@ -1,10 +1,25 @@
 defmodule UrlShortnerWeb.UrlView do
   use UrlShortnerWeb, :view
 
+  @one_line_string 56
+
   @doc """
   This fn returns the host + the slug of the URL to show to the user the URL shortned
   """
   def build_link_text(slug) do
     "#{UrlShortnerWeb.Endpoint.url()}/#{slug}"
+  end
+
+  @doc """
+  This fn check the lenght of the URL, if the URL is greather than @one_line_string it short to fit better on the screen
+  """
+  def format_original_url(url) do
+    splitted_url = String.graphemes(url)
+
+    if Enum.count(splitted_url) > @one_line_string do
+      "#{Enum.take(splitted_url, @one_line_string)}..."
+    else
+      url
+    end
   end
 end
--- test/url_shortner_web/views/url_view_test.exs
+++ test/url_shortner_web/views/url_view_test.exs
@@ -4,16 +4,31 @@ defmodule UrlShortnerWeb.UrlViewTest do
   doctest UrlShortnerWeb.UrlView
   alias UrlShortnerWeb.UrlView
 
-  alias UrlShortner.{Repo, Url}
-
   describe "build_link_text/1" do
     test "it should returns the entire url shortned to show to the user" do
       slug = "xkcd327"
-      Repo.insert(%Url{original_url: "https://google.com", slug: slug})
 
       result = UrlView.build_link_text(slug)
 
       assert result == "http://localhost:4000/#{slug}"
+    end
+  end
+
+  describe "format_original_url/1" do
+    test "when the url is big than our reference value, it should short it and put ellipsis" do
+      url = "https://www.google.com/search?q=busca+de+uma+url+bem+grande+no+g"
+
+      result = UrlView.format_original_url(url)
+
+      assert result == "https://www.google.com/search?q=busca+de+uma+url+bem+gra..."
+    end
+
+    test "when the url is shorter than our reference value, it should return the full url without ellipsis" do
+      url = "https://www.google.com/"
+
+      result = UrlView.format_original_url(url)
+
+      assert result == "https://www.google.com/"
     end
   end
 end
